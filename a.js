452b792a7b1d6c7d3474adec61a244daf10bbe0a
Kimikazu Kato <kimikazu@gmail.com>
2022-07-29 20:08:24 +0900

    Slight change for pad string

--- a.js
+++ a.js
@@ -11,7 +11,7 @@ function obfuscate(s, start, end, pads) {
 
 function fill() {
     const text =  "時代の進展に伴い、国民経済の下降圧力を背景に、新たな投資手法を模索し、高いリターンが得られるかどうかは投資手法は重要です。時代の大きな変化の下で富の秘訣は教師の手にあり、毎月の安定収入は30％以上です。私たちの高収入の秘訣は、ウォールストリートの投資および財務管理チームであるということです。\nプロなので安心してください。\nチームへの参加と連絡先の詳細については私のプロフィールをクリックしてください。";
-    const pads = "ᢦ᠂᠈゛";
+    const pads = "ᢦ᠂᠈ﾞ";
     let result = obfuscate(text,0,145,pads).replace(/\n/g,"<br>");
     const element = document.getElementById("content")
     element.innerHTML = result;
